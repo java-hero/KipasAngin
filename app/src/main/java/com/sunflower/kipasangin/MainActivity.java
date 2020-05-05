3ee11c597a78ac645b8a942be75c6432b3d1afc2
Fiqry choerudin <fiqrychoerudin@student.telkomuniversity.ac.id>
2020-05-06 00:26:05 +0700

    Add Custom Circle button

--- app/src/main/java/com/sunflower/kipasangin/MainActivity.java
+++ app/src/main/java/com/sunflower/kipasangin/MainActivity.java
@@ -3,21 +3,32 @@ package com.sunflower.kipasangin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     Animation rotationAnimation;
     ImageView imageView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageView = findViewById(R.id.imageView);
-        rotateAnimation();
+        imageView = findViewById(R.id.kipasBlade);
+        button = findViewById(R.id.buttonOn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rotateAnimation();
+            }
+        });
+
     }
 
     private void rotateAnimation() {
