9c86cc286ad7b6043136dea55a15307cc9e80068
Fiqry choerudin <fiqrychoerudin@student.telkomuniversity.ac.id>
2020-05-06 00:00:23 +0700

    add kipas body

--- app/src/main/java/com/sunflower/kipasangin/MainActivity.java
+++ app/src/main/java/com/sunflower/kipasangin/MainActivity.java
@@ -3,13 +3,25 @@ package com.sunflower.kipasangin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Animation rotationAnimation;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+        rotateAnimation();
+    }
 
+    private void rotateAnimation() {
+        rotationAnimation = AnimationUtils.loadAnimation(this,R.anim.rotate_animation);
+        imageView.startAnimation(rotationAnimation);
     }
 }
